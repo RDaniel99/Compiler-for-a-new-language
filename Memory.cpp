42d7153ad70d2bae9c2bc9df346e8d17cee93f2e
Stefan <stefan.tomsa99@gmail.com>
2020-01-04 16:46:43 +0200

    __

--- Memory.cpp
+++ Memory.cpp
@@ -72,23 +72,17 @@ bool Memory::Exista(functie f, int &pos)
 
 bool Memory::Exista(variabila v, int &pos)
 {
-    cout<<"\n\n\nEXISTA APELAT PE VAR "<<v.nume<<"\n";
     pos = -1;
 
-    cout<<"START MEMORIE\n";
     for(unsigned int i = 0; i < variabile.size(); i++)
     {
-        cout<<variabile[i].nume<<"\n";
         if(variabile[i].nume == v.nume)
         {
             pos = i;
-
             return true;
         }
     
     }
-    cout<<"END MEMORIE\n\n\n";
-
     return false;
 }
 
@@ -98,7 +92,6 @@ bool Memory::Exista(clasa c, int &pos)
 
     for(unsigned int i = 0; i < clase.size(); i++)
     {
-        
         if(clase[i].nume == c.nume)
         {
             pos = i;
